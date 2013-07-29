bc186a77a61be3560109738297309feffb54915d
Val Neekman <val@neekware.com>
2013-07-28 20:49:47 -0400

    add progress bar

--- geoware/management/commands/wiki.py
+++ geoware/management/commands/wiki.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import logging
+import resource
+import progressbar
 from optparse import make_option
 from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext as _
@@ -16,7 +18,7 @@ from ... import defaults
 from ...models import (Ocean, Continent, Country, Region, Subregion,
     City, District, Language, Currency, Timezone)
 
-logger = logging.getLogger("geoware.cmd.wikipedia")
+logger = logging.getLogger("geoware.cmd.wiki")
 
 class Command(BaseCommand):
     cmd_name = 'Wiki'
@@ -32,6 +34,23 @@ class Command(BaseCommand):
         ['City', City],
         ['District', District],
     ]
+
+    def _progress_widget(self, name):
+        class MemoryUsageWidget(progressbar.ProgressBarWidget):
+            def update(self, pbar):
+                return '{0} kB'.format(str(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss).rjust(8))
+
+        widgets = [
+            '{0}|RAM usage:'.format("Wiki |Fetching:  {0}".format((name[:10] + (name[10:] and '..')).rjust(10))),
+            MemoryUsageWidget(),
+            '|',
+            progressbar.ETA(),
+            '|Done:',
+            progressbar.Percentage(),
+            progressbar.Bar(),
+        ]
+        return widgets
+
     option_list = BaseCommand.option_list + (
         make_option('-o', '--overwrite', action='store_true', default=False,
             help='Overwrite any locally modified data with the new fetched data.'
@@ -71,31 +90,50 @@ class Command(BaseCommand):
                     if a.lower() == b[0].lower():
                         search_types_list.append(b)
 
-        for klass in search_types_list:
-            self.fetch_info_for_type(klass[1])
-    
-    def fetch_info_for_type(self, klass):
+        for stype in search_types_list:
+            self.fetch_info_for_type(stype)
+
+    def fetch_info_for_type(self, search_type):
         name_q = Q()
         for name in self.search_names:
             name_q |= Q(**{"name__iexact": name})
+
+        search_name, search_klass = search_type
         if self.overwrite:
-            instances = klass.objects.filter(is_active=True).filter(name_q)
+            items = search_klass.objects.filter(is_active=True).filter(name_q).order_by('name')
+            total_rows = search_klass.objects.filter(is_active=True).filter(name_q).count()
         else:
             query_url = get_not_null_and_not_blank_query('url')
             query_info = get_null_or_blank_query('info')
-            instances = klass.objects.filter(is_active=True).filter(query_url).filter(query_info).filter(name_q)
-        for instance in instances:
-            info = self.fetch_wikipedia_summary(instance)
-            instance.info = info
-            instance.save()
-    
+            items = search_klass.objects.filter(is_active=True).filter(query_url).filter(query_info).filter(name_q).order_by('name')
+            total_rows = search_klass.objects.filter(is_active=True).filter(query_url).filter(query_info).filter(name_q).count()
+
+        loop_counter = 0
+        row_count = 0
+        progress = progressbar.ProgressBar(maxval=total_rows, widgets=self._progress_widget(search_name))
+        for item in items:
+            row_count += 1; progress.update(row_count)
+            try:
+                item.info = self.fetch_wikipedia_summary(item)
+            except:
+                logger.error(_("Failed to fetch. ({0})\n").format(item.name))
+                continue
+            else:
+                logger.debug(_("Fetched. ({0})\n").format(item.name))
+            item.save()
+            if loop_counter == 500:
+                loop_counter = 0
+                reset_queries()
+            loop_counter = loop_counter + 1
+
     def fetch_wikipedia_summary(self, obj):
-        summary = get_wiki_summary(title=obj.name)
+        summary = ''
+        if obj.url and 'wikipedia' in obj.url:
+            title = obj.url.split('/')[-1]
+            summary = get_wiki_summary(title=title)
         if not summary:
-            if obj.url and 'wikipedia' in obj.url:
-                title = obj.url.split('/')[-1]
-                summary = get_wiki_summary(title=title)
-        print summary, "\n"
+            summary = get_wiki_summary(title=obj.name)
+        # print summary + "\n"
         return summary.strip()
 
 
