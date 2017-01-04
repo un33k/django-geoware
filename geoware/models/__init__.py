4fa92f6d6c6a85fbb22dd169858b77beea2ed843
Val Neekman <val@neekware.com>
2017-01-04 12:19:08 -0500

    ongoing ...

--- geoware/models/__init__.py
+++ geoware/models/__init__.py
@@ -1,25 +1,11 @@
-import os
-import re
-import types
-
-PACKAGE = 'geoware.models'
-MODULE_RE = r"^.*.py$"
-SKIP = ['.', '..', '__init__.py']
-
-# Examine every file inside this module
-submodules = []
-module_dir = os.path.dirname( __file__)
-for fname in os.listdir(module_dir):
-    if fname not in SKIP and re.match(MODULE_RE, fname):
-        module = __import__('{0}.{1}'.format(PACKAGE, fname[:-3]), {}, {}, fname[:-3])
-        for name in dir(module):
-            item = getattr(module, name)
-            if isinstance(item, (type, types.ClassType)):
-                # matched! include in module's namespace.
-                exec '{0} = item'.format(name)
-                submodules.append(name)
-
-# Only reveals what each module allows us to see. Enforced by __all__ in each file
-__all__ = submodules
-
-
+from .continent import Continent
+from .country import Country
+from .region import Region
+from .subregion import Subregion
+from .city import City
+from .district import District
+from .ocean import Ocean
+from .language import Language
+from .currency import Currency
+from .timezone import Timezone
+from .altname import Altname
