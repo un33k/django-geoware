8916eb73f055ab3451edd3fad1fee78fe84d85dd
Val Neekman <val@neekware.com>
2017-01-04 11:23:37 -0500

    ongoing ...

--- geoware/models/region.py
+++ geoware/models/region.py
@@ -1,24 +1,52 @@
-from django.db import models
-from django.utils.translation import gettext as _
-from base import LocationBase
+from django.utils.translation import ugettext_lazy as _
 
-__all__ = ['Region']
+from .base import models
+from .base import AbstractLocation
 
-class Region(LocationBase):
-    country = models.ForeignKey('Country', related_name='%(app_label)s_%(class)s_country', null=True, blank=True)
-    capital = models.ForeignKey('City', related_name='%(app_label)s_%(class)s_capital', null=True, blank=True)
-    code = models.CharField(_('Code'), max_length=40, null=True, blank=True)
-    fips = models.CharField(_('FIPS code'), max_length=40, blank=True, null=True)
 
-        
+class Region(AbstractLocation):
+    """
+    Region Model Class.
+    """
+    country = models.ForeignKey(
+        'Country',
+        _('LOCATION.REGION.COUNTRY')
+        related_name='%(app_label)s_%(class)s_country',
+        null=True,
+        blank=True,
+    )
+
+    capital = models.ForeignKey(
+        'City',
+        _("LOCATION.REGION.CAPITAL"),
+        related_name='%(app_label)s_%(class)s_capital',
+        null=True,
+        blank=True,
+    )
+
+    code = models.CharField(
+        _('LOCATION.REGION.CODE'),
+        max_length=40,
+        null=True,
+        blank=True,
+    )
+
+    code = models.CharField(
+        _('LOCATION.REGION.CODE_FIPS'),
+        max_length=40,
+        null=True,
+        blank=True,
+    )
+
     class Meta:
         app_label = 'geoware'
-        db_table = app_label + '-region'
+        db_table = '{app}-{type}'.format(app=app_label, type='region')
+        verbose_name = _('LOCATION.REGION')
+        verbose_name_plural = _('LOCATION.REGION#plural')
         unique_together = [('name', 'country')]
 
-    @property
+   @property
     def parent(self):
         if self.country:
             return self.country
         return None
-
