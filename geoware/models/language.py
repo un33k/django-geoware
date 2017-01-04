0e72ad349b4aafee5e096653192151e4833e5938
Val Neekman <val@neekware.com>
2017-01-04 11:19:31 -0500

    ongoing ...

--- geoware/models/language.py
+++ geoware/models/language.py
@@ -1,39 +1,79 @@
-from django.db import models
-from django.utils.translation import gettext as _
-from django.utils.encoding import force_unicode
+from django.utils.translation import ugettext_lazy as _
 
-from .. import defaults
+from slugify import slugify
 
-__all__ = ['Language']
+from .base import models
+from .base import AbstractLocation
+
 
 class Language(models.Model):
+    """
+    Language Model Class.
+    """
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
-    name = models.CharField(_('Language name'), max_length=254, blank=True, null=True)
-    code = models.CharField(_("Language code"), max_length=40, db_index=True)
-    slug = models.CharField(_('Slug'), max_length=254, blank=True, null=True)
-    percent = models.FloatField(_('Worldwide percentage'), blank=True, null=True)
-    dialect = models.CharField(max_length=254, blank=True, null=True)
-    url = models.URLField(_("URL"), max_length=254, blank=True, null=True)
-    info = models.TextField(_('Information Details'), blank=True)
-    is_active = models.BooleanField(_('Active'), default=True)
+    name = models.CharField(
+        _("LOCATION.LANGUAGE.NAME"),
+        max_length=254,
+        null=True,
+        blank=True,
+    )
 
-    def __unicode__(self):
-        return force_unicode(self.name)
-        
+    slug = models.CharField(
+        _('LOCATION.LANGUAGE.SLUG'),
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    code = models.CharField(
+        _("LOCATION.LANGUAGE.CODE"),
+        db_index=True,
+        max_length=40,
+    )
+
+    percent = models.FloatField(
+        _("LOCATION.LANGUAGE.WORLDWIDE_PERCENTAGE"),
+        null=True,
+        blank=True,
+    )
+
+    dialect = models.CharField(
+        _("LOCATION.LANGUAGE.DIALECT"),
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    url = models.URLField(
+        _('LOCATION.LANGUAGE.URL'),
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    info = models.TextField(
+        _('LOCATION.LANGUAGE.INFO_DETAILS'),
+        null=True,
+        blank=True,
+    )
+
+    is_active = models.BooleanField(
+        _('LOCATION.LANGUAGE.ACTIVE'),
+        default=True,
+    )
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         app_label = 'geoware'
-        db_table = app_label + '-language'
-        # ordering = ('name', 'dialect', 'slug', 'percent',)
+        db_table = '{app}-{type}'.format(app=app_label, type='language')
+        verbose_name = _('LOCATION.LANGUAGE')
+        verbose_name_plural = _('LOCATION.LANGUAGE#plural')
+        unique_together = (('name','code'), )
 
     def save(self, *args, **kwargs):
-        if self.name:
-            s = self.name
-            if self.dialect:
-                s += '-' + self.dialect
-            self.slug = defaults.slugify(self.name)
-        super(Language, self).save(*args, **kwargs)
-
-
-
+        self.slug = slugify('{name}-{code}'.format(name=self.name, code=self.code))
+        super().save(*args, **kwargs)
