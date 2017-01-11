95c0ce560ec814fffdefea617ff07ecb52595b79
Val Neekman <val@neekware.com>
2017-01-11 15:18:54 -0500

    progress

--- geoware/management/utils/loaders.py
+++ geoware/management/utils/loaders.py
@@ -5,33 +5,47 @@ from ...models import Language
 from ... import defaults as defs
 
 
-def load_continents(force=False):
+def save_object_attrs(instance, data, created=False):
+    """
+    Given an instance and a data dictionary, it loads the data and saves the instance.
+    """
+    if not created:
+        for attr, value in data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+
+def load_continents():
     """
     Load the continents.
     """
-    for continent in defs.GEOWARE_GEONAME_CONTINENT_DATA:
-        Continent.objects.get_or_create(code=continent['code'], defaults=continent)
+    for data in defs.GEOWARE_GEONAME_CONTINENT_DATA:
+        instance, created = Continent.objects.get_or_create(code=data['code'], defaults=data)
+        save_object_attrs(instance, data, created)
 
 
 def load_oceans(force=False):
     """
     Load the oceans.
     """
-    for ocean in defs.GEOWARE_GEONAME_OCEAN_DATA:
-        Ocean.objects.get_or_create(name=ocean['name'], defaults=ocean)
+    for data in defs.GEOWARE_GEONAME_OCEAN_DATA:
+        instance, created = Ocean.objects.get_or_create(name=data['name'], defaults=data)
+        save_object_attrs(instance, data, created)
 
 
 def load_currencies(force=False):
     """
     Load the currencies.
     """
-    for currency in defs.GEOWARE_CURRENCY_DATA:
-        Currency.objects.get_or_create(code=currency['code'], defaults=currency)
+    for currency, data in defs.GEOWARE_CURRENCY_DATA.items():
+        instance, created = Currency.objects.get_or_create(code=data['code'], defaults=data)
+        save_object_attrs(instance, data, created)
 
 
 def load_languages(force=False):
     """
     Load the languages.
     """
-    for language in defs.GEOWARE_LANGUAGE_DATA:
-        Language.objects.get_or_create(code=language['code'], defaults=language)
+    for data in defs.GEOWARE_LANGUAGE_DATA:
+        instance, created = Language.objects.get_or_create(code=data['code'], defaults=data)
+        save_object_attrs(instance, data, created)
