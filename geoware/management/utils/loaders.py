f863d60e9d82863f47b15ea1915accb93671351b
Val Neekman <val@neekware.com>
2017-01-11 15:07:02 -0500

    progress

--- geoware/management/utils/loaders.py
+++ geoware/management/utils/loaders.py
@@ -10,7 +10,7 @@ def load_continents(force=False):
     Load the continents.
     """
     for continent in defs.GEOWARE_GEONAME_CONTINENT_DATA:
-        Continent.objects.get_or_create(**continent)
+        Continent.objects.get_or_create(code=continent['code'], defaults=continent)
 
 
 def load_oceans(force=False):
@@ -18,15 +18,15 @@ def load_oceans(force=False):
     Load the oceans.
     """
     for ocean in defs.GEOWARE_GEONAME_OCEAN_DATA:
-        Continent.objects.get_or_create(**continent)
+        Ocean.objects.get_or_create(name=ocean['name'], defaults=ocean)
 
 
 def load_currencies(force=False):
     """
     Load the currencies.
     """
-    for ocean in defs.GEOWARE_CURRENCY_DATA:
-        Currency.objects.get_or_create(currency['code']).update(**currency)
+    for currency in defs.GEOWARE_CURRENCY_DATA:
+        Currency.objects.get_or_create(code=currency['code'], defaults=currency)
 
 
 def load_languages(force=False):
@@ -34,4 +34,4 @@ def load_languages(force=False):
     Load the languages.
     """
     for language in defs.GEOWARE_LANGUAGE_DATA:
-        Language.objects.get_or_create(language['code']).update(**language)
+        Language.objects.get_or_create(code=language['code'], defaults=language)
