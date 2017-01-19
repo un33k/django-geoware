8d2ef27f0695a7d71f6a1e1bf5b9b2dddb760625
Val Neekman <val@neekware.com>
2017-01-19 12:05:42 -0500

    enable laguages

--- geoware/admin/__init__.py
+++ geoware/admin/__init__.py
@@ -4,39 +4,9 @@ from .country import *
 from .division import *
 from .subdivision import *
 from .city import *
+from .language import *
 
-
-# from django.contrib import admin
-
-# from ..models import Continent
-# from ..models import Country
-# from ..models import Division
-# from ..models import Subdivision
-# from ..models import City
-# from ..models import Ocean
 # from ..models import Language
 # from ..models import Currency
 # from ..models import Timezone
 # from ..models import Altname
-
-# from .continent import ContinentAdmin
-# from .country import CountryAdmin
-# from .division import DivisionAdmin
-# from .subdivision import SubdivisionAdmin
-# from .city import CityAdmin
-# from .ocean import OceanAdmin
-# from .language import LanguageAdmin
-# from .currency import CurrencyAdmin
-# from .timezone import TimezoneAdmin
-# from .altname import AltnameAdmin
-
-# admin.site.register(Ocean, OceanAdmin)
-# admin.site.register(Currency, CurrencyAdmin)
-# admin.site.register(Language, LanguageAdmin)
-# admin.site.register(Continent, ContinentAdmin)
-# admin.site.register(Country, CountryAdmin)
-# admin.site.register(Division, DivisionAdmin)
-# admin.site.register(Subdivision, SubdivisionAdmin)
-# admin.site.register(City, CityAdmin)
-# admin.site.register(Timezone, TimezoneAdmin)
-# admin.site.register(Altname, AltnameAdmin)
